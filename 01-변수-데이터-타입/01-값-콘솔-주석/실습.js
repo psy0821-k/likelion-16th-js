5b7ab4ed83330abae66418012500ae1331102fa4
psy0821-k <p49417875@gmail.com>
2025-12-26 13:40:01 +0900

    자바스크립트 데이터 타입

--- 01-변수-데이터-타입/01-값-콘솔-주석/실습.js
+++ 01-변수-데이터-타입/01-값-콘솔-주석/실습.js
@@ -1,12 +1,17 @@
-숫자(number) 값
+// 숫자(number) 값
+100;
+10.5;
+-10;
+// 문자(string) 값
+'문자';
+"문자";
+`문자`;
+// 논리(boolean) 값
+true;
+false;
+// 터미널 값 출력
 
-문자(string) 값
+// 주석 한 줄 (single line comment)
 
-논리(boolean) 값
-
-터미널 값 출력
-
-주석 한 줄 (single line comment)
-
-여러 줄
-주석 작성 (multi line comment)
+// 여러 줄
+// 주석 작성 (multi line comment)
